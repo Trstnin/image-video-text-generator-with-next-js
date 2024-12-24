baf85b44d50a6c616b62cd8b8110f2253a2792e9
Trstnin <152942538+Trstnin@users.noreply.github.com>
2024-12-24 14:52:41 +0000

    Frontend setup work

---
+++ ai-short-video-generator/src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <div className="bg-black h-screen">
+  <h1 className="text-white text-center mx-5 p-3">Ai-short-video-generator</h1>
+  <Button>Get Started</Button>
+
+  </div>
+  )
+}
